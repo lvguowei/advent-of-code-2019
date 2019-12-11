a80331ec6571a260fa8ccce705272ef98d4b47db
Guowei Lv <lvguowei1002@gmail.com>
2019-12-11 22:01:12 +0200

    1-2

--- 1/app/src/app/core.clj
+++ 1/app/src/app/core.clj
@@ -1,12 +1,36 @@
 (ns app.core)
 
-(defn -main
-  ""
+(defn read-input
   []
+  "Returns list of masses"
   (let [input (slurp "resources/input.txt")
         masses (clojure.string/split-lines input)]
+    (map #(Integer/parseInt %) masses)))
+
+(defn fuel
+  [mass]
+  (- (Math/floor (/ mass 3)) 2))
+
+(defn fuel-r
+  ""
+  [mass]
+  (loop [m mass
+         s 0]
+    (let [f (fuel m)]
+      (if (<= f 0)
+        s
+        (recur f (+ s f))))))
+
+(defn start
+  ""
+  []
+  (let [masses (read-input)]
     (reduce
      +
-     (map
-      #(- (Math/floor (/ (Integer/parseInt %) 3)) 2)
-      masses))))
+     (map fuel masses))))
+
+(defn start2
+  ""
+  []
+  (let [masses (read-input)]
+    (reduce + (map fuel-r masses))))
